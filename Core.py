5d1d140bf62d370e4873ecd29b40e98f36c1f31c
Red-Rainbow <Red-Rainbow@users.noreply.github.com>
2013-11-10 17:01:30 +0100

    Updated resource calculation and added all resource calc factors to nation class.

--- Core.py
+++ Core.py
@@ -4,8 +4,11 @@ import time
 
 # Nation class
 class Nation:
-    Resources = 0
     Production = 0
+    Technology = 0
+    Trade = 0
+    Resources = 0
+    Overspent = 0
     NationName = str
     LeaderName = str
 
@@ -32,7 +35,7 @@ for Round in range(1,RoundLimit+1):
     for Turn in range(1,NationQuantity+1):
 
         # Calculate and display resources for nation
-        NationArray[Turn-1].Resources = 5+NationArray[Turn-1].Production+NationArray[Turn-1].Resources
+        NationArray[Turn-1].Resources = 5 + NationArray[Turn-1].Production + ((NationArray[Turn-1].Technology + NationArray[Turn-1].Trade) / 10) + NationArray[Turn-1].Resources - ((NationArray[Turn-1].Resources**2) / 1000) - (2 * NationArray[Turn-1].Overspent)
         print("R"+str(Round)+"T"+str(Turn)+": "+str(NationArray[Turn-1].NationName)+" may spend "+str(NationArray[Turn-1].Resources)+" resources.")
 
         # End of turn, pause for 1 second
