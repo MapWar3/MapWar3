ed43cb87e6b2b587cd0f414935116e9c38994a3e
Red-Rainbow <Red-Rainbow@users.noreply.github.com>
2013-11-10 15:27:54 +0100

    Read nation and leader names from keyboard input

--- Core.py
+++ Core.py
@@ -1,13 +1,20 @@
+# Imports
 import os
 
+# Initial settings
 NationQuantity = 3
 RoundLimit = 5
 Round = int
 Turn = int
-NationName = "NationName"
-LeaderName = "LeaderName"
-print("Turn = "+str(Turn)+" Round = "+str(Round)+" NationName = "+NationName+" LeaderName = "+LeaderName)
 
+# Get nation and leader name from keyboard input
+NationName = raw_input('Type the name of your nation: ')
+LeaderName = raw_input("What is your name, mighty leader of "+NationName+"?: ")
+
+# Display leader and nation name
+print(" NationName = "+NationName+" LeaderName = "+LeaderName)
+
+# Rounds and turns loops
 for Round in range(1,RoundLimit+1):
     for Turn in range(1,NationQuantity+1):
         print("Round "+str(Round)+", Turn "+str(Turn))
