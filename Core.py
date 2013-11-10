19f95c571a9432d83eb6d155a57bc4f87f525def
Red-Rainbow <Red-Rainbow@users.noreply.github.com>
2013-11-10 19:40:59 +0100

    No longer uses empty nation/leader names

--- Core.py
+++ Core.py
@@ -1,9 +1,10 @@
 #Must have NationNames.txt and LeaderNames.txt in the same directory as this program to run
 
-# Imports
+# Imports and initial stuff
 import os
 import random
 import time
+random.seed()
 
 # Nation class
 class Nation:
@@ -18,22 +19,36 @@ class Nation:
 # Initial settings
 Round = int
 Turn = int
+SleepTime = float
+DebugVar = input("Debug mode? (1 = yes, 0 = no): ")
 NationQuantity = input("How many will be playing?: ")
 RoundLimit = input("The maximum number of rounds?: ")
+
+# Sets wait time after turn based on debug mode on/off
+if DebugVar == 1:
+    SleepTime = 0.1
+else:
+    SleepTime = 1
 
 # Load nationnames from NationNames.txt
 text_file = open("NationNames.txt", "r")
 NationNameDatabase = text_file.read().split("\n")
 text_file.close()
-print NationNameDatabase
+if NationNameDatabase[len(NationNameDatabase)-1] == "":
+    NationNameDatabase.pop(len(NationNameDatabase)-1)
 NationNameQuantity = len(NationNameDatabase)
 
 # Load leadernames from LeaderNames.txt
 text_file = open("LeaderNames.txt", "r")
 LeaderNameDatabase = text_file.read().split("\n")
 text_file.close()
-print LeaderNameDatabase
+if LeaderNameDatabase[len(LeaderNameDatabase)-1] == "":
+    LeaderNameDatabase.pop(len(LeaderNameDatabase)-1)
 LeaderNameQuantity = len(LeaderNameDatabase)
+
+if DebugVar == 1: # Show loaded nation and leader names if debug mode is on.
+    print NationNameDatabase
+    print LeaderNameDatabase
 
 # Create array of nations based on keyboard input
 NationArray = []
@@ -45,23 +60,24 @@ NationArray[0].NationName = raw_input('Type the name of your nation: ')
 NationArray[0].LeaderName = raw_input("What is your name, mighty leader of "+NationArray[0].NationName+"?: ")
 
 # Set random AI nation and leader names
-random.seed()
 for Turn in range(1,NationQuantity):
     NationArray[Turn].NationName = NationNameDatabase[random.randint(0,NationNameQuantity-1)]
     NationArray[Turn].LeaderName = LeaderNameDatabase[random.randint(0,LeaderNameQuantity-1)]
 
 # Display leader and nation name
-print(" NationName = "+NationArray[0].NationName+" LeaderName = "+NationArray[0].LeaderName)
+print("You established "+NationArray[0].NationName+".")
 
 # Rounds and turns loops
 for Round in range(1,RoundLimit+1):
     for Turn in range(1,NationQuantity+1):
+        if (Round == 1) and (Turn > 1):
+            print(NationArray[Turn-1].LeaderName+" established "+NationArray[Turn-1].NationName+".")
 
         # Calculate and display resources for nation
         NationArray[Turn-1].Resources = 5 + NationArray[Turn-1].Production + ((NationArray[Turn-1].Technology + NationArray[Turn-1].Trade) / 10) + NationArray[Turn-1].Resources - ((NationArray[Turn-1].Resources**2) / 1000) - (2 * NationArray[Turn-1].Overspent)
         print("R"+str(Round)+"T"+str(Turn)+": "+str(NationArray[Turn-1].NationName)+" may spend "+str(NationArray[Turn-1].Resources)+" resources.")
 
         # End of turn, pause for 1 second
-        time.sleep(1)
+        time.sleep(SleepTime)
 
 os.system("pause") # Doesn't close out until clicked
