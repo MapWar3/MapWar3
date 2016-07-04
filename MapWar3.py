305446dfa633ac7b11941e5508a954f92b171776
Bong Lotz <blotz9001@gmail.com>
2016-07-04 11:20:30 -0400

    fix'd a lot of button stuff

--- MapWar3.py
+++ MapWar3.py
@@ -29,8 +29,8 @@ def button(msg,x,y,w,h,ic,ac,action=None): # Button function: Message, position 
     mousePos = pygame.mouse.get_pos() # (x,y) of cursor relative to top left of display
     mouseClick = pygame.mouse.get_pressed() # (l,c,r), 0 or 1 for left, center or right mouse button clicked
     if x+w > mousePos[0] > x and y+h > mousePos[1] > y: # Mouse is over button
-        pygame.draw.rect(intro,ac,(x,y,w,h))
-        pygame.draw.rect(intro,black,(x+5,y+5,w-10,h-10))
+        pygame.draw.rect(screen,ac,(x,y,w,h))
+        pygame.draw.rect(screen,black,(x+5,y+5,w-10,h-10))
         textSurf, textRect = textObjects(msg,buttonFont,ac) # Button text
         if mouseClick[0] ==  1:
             print("button clicked, function!!!")
@@ -42,8 +42,8 @@ def button(msg,x,y,w,h,ic,ac,action=None): # Button function: Message, position 
                 pygame.quit()
                 quit()
     else: # Mouse is not over button
-        pygame.draw.rect(intro,ic,(x,y,w,h))
-        pygame.draw.rect(intro,black,(x+5,y+5,w-10,h-10))
+        pygame.draw.rect(screen,ic,(x,y,w,h))
+        pygame.draw.rect(screen,black,(x+5,y+5,w-10,h-10))
         textSurf, textRect = textObjects(msg,buttonFont,ic) # Button text
     textRect.center = (x+w/2,y+h/2) # Center point of button
     screen.blit(textSurf, textRect) # Render button text
@@ -51,10 +51,9 @@ def button(msg,x,y,w,h,ic,ac,action=None): # Button function: Message, position 
 # Putting the initial screen up
 screen.blit(intro, (0, 0))
 mousePos = pygame.mouse.get_pos() # (x,y) of cursor relative to top left of display
-button("Start!",25,693,200,50,red,green,"startGame") # Start game button
 pygame.display.flip()
-
 while(screen_id == 0): # Initial screen
+    button("Start!",25,693,200,50,red,green,"startGame")
     pressed = pygame.key.get_pressed()
     mousePos = pygame.mouse.get_pos() # (x,y) of cursor relative to top left of display
     mouseClick = pygame.mouse.get_pressed() # (l,c,r), 0 or 1 for left, center or right mouse button clicked
@@ -79,7 +78,6 @@ while(screen_id == 0): # Initial screen
         if event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 1: # Left mouse button
                 print "lmb!"
-                button("Start!",25,693,200,50,red,green,"startGame") # Start button
             elif event.button == 3: # Right mouse button
                 print "rmb!"
         
@@ -87,13 +85,12 @@ while(screen_id == 0): # Initial screen
             # if mouse moved, add point to list 
 			#very messy hack to display coordinates at to left corner, fix later
 			#works by displaying the coordinates, then adding another intro picture, repeat, etc.
-            screen.blit(intro, (0,0))
-            mouseposDisp = xyfont.render(str(pygame.mouse.get_pos()[0])+", "+str(pygame.mouse.get_pos()[1]), 0, (255,255,0))
-            screen.blit(mouseposDisp, (0, 0))
-            print "Moved!"
-
-            button("Start!",25,693,200,50,red,green,"startGame") # Start button
-            
+            #screen.blit(intro, (0,0))
+            #mouseposDisp = xyfont.render(str(pygame.mouse.get_pos()[0])+", "+str(pygame.mouse.get_pos()[1]), 0, (255,255,0))
+            #screen.blit(mouseposDisp, (0, 0))
+            #print "Moved!"
+            pass
+			
     #screen.fill((0, 0, 0)) # Black screen
     pygame.display.flip()
     #pygame.display.update()
